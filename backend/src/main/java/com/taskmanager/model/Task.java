83e7bdd6e23c6024c905a8d12c1c627e1f97d171
Amann <91307961+Amannpy@users.noreply.github.com>
2025-06-23 13:52:05 +0000

    feat: add Task entity with JPA annotations

---
+++ backend/src/main/java/com/taskmanager/model/Task.java
@@ -0,0 +1,85 @@
+package com.taskmanager.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private int priority; //1 = High, 2 = Medium, 3 = Low
+
+    private LocalDate duDate;
+
+    private boolean completed;
+
+    // --- Constructors ---
+    public Task() {}
+
+    public Task(String title, String description, int priority, LocalDate duDate, boolean completed){
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.duDate = duDate;
+        this.completed = completed;
+    }
+
+    // --- Getters and Setters ---
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public int getPriority(){
+        return priority;
+    }
+
+    public void setPriority(int priority){
+        this.priority = priority;
+    }
+
+    public LocalDate getdueDate(){
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate){
+        this.duDate =  dueDate;
+    }
+
+    public boolean isCompleted(){
+        return completed;
+    }
+
+    public void setCompleted(boolean completed){
+        this.completed = completed;
+    }
+}
